1767eb279ac7df7294465017170c09d578821e04
Florian Friedrich <ffried@me.com>
2019-08-03 00:22:05 +0200

    generate linux main test files

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,10 @@
 import XCTest
 
 import SwiftSMTPTests
+import SwiftSMTPVaporTests
 
 var tests = [XCTestCaseEntry]()
-tests += SwiftSMTPTests.allTests()
+tests += SwiftSMTPTests.__allTests()
+tests += SwiftSMTPVaporTests.__allTests()
+
 XCTMain(tests)
