97e400dc96ac52982bdd0c05101fec264e38aea2
Florian Friedrich <florian.friedrich@sersoft.de>
2026-06-25 12:01:07 +0200

    Move to compile-time checked regex

--- Sources/SwiftSMTP/Helpers/Email+Validation.swift
+++ Sources/SwiftSMTP/Helpers/Email+Validation.swift
@@ -1,15 +1,14 @@
 fileprivate import Foundation
 
-// Regex taken from https://emailregex.com
-private let emailRegexPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
 private func validateEmailAddress(_ address: String) throws {
     let matches: Bool
+    // Regex taken from https://emailregex.com
     if #available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *) {
-        let emailRegex = try Regex(emailRegexPattern)
+        // TODO: Make static once the minimum platformds allow it.
+        let emailRegex = /[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}/
         matches = try emailRegex.wholeMatch(in: address) != nil
     } else {
-        let emailRegex = try NSRegularExpression(pattern: emailRegexPattern)
+        let emailRegex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
         matches = emailRegex.numberOfMatches(in: address, options: .anchored, range: NSRange(address.startIndex..., in: address)) == 1
     }
     if !matches {
